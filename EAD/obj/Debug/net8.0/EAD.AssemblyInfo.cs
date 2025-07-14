511e65a4fafab53f2db7bcf22cccd4a8c94def82
Sara Monteiro <saramonteiro@gmail.com>
2025-07-14 13:59:44 -0300

    BUGFIX

--- EAD/obj/Debug/net8.0/EAD.AssemblyInfo.cs
+++ EAD/obj/Debug/net8.0/EAD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a37908bdf19712b4ef14591f1449a6425e5cf8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ababc14bad9963f3b644e36895ef9669f1905b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
